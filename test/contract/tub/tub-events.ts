b42d291675c1769beb683c70f1de9d5a6dd71750
CJ <CjHare@users.noreply.github.com>
2022-02-09 10:57:44 +1000

    feat: minimal footprint setup

---
+++ test/contract/tub/tub-events.ts
@@ -0,0 +1,19 @@
+import {Event} from 'ethers'
+import {expect} from 'chai'
+import {StoreEvent} from '../../../typechain-types/Tub'
+
+/**
+ * Validates the event shape and converts the arguments of a StoreEvent.
+ *
+ * @param event whose content is expected to match a StoreEvent.
+ *              If expectation are not met, test will be failed.
+ */
+export function storeEvent(event: Event): {value: string} {
+    const store = event as StoreEvent
+    expect(store.args).is.not.undefined
+
+    const args = store.args
+    expect(args?.value).is.not.undefined
+
+    return args
+}
