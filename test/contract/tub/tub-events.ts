c98bee9df8b0f0274730c63ffdb2848901644957
CJ <CjHare@users.noreply.github.com>
2022-09-09 12:05:10 +1000

    fixes

--- test/contract/tub/tub-events.ts
+++ test/contract/tub/tub-events.ts
@@ -1,6 +1,6 @@
 import {Event} from 'ethers'
 import {expect} from 'chai'
-import {StoreEvent} from '../../../typechain-types/Tub'
+import {StoreEvent} from '../../../typechain-types/contracts/test/Tub'
 
 /**
  * Validates the event shape and converts the arguments of a StoreEvent.
